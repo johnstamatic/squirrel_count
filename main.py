bf33df016f7d2a2538e967445eb4c802ce1a5797
johnstamatic <awjohnson@gmail.com>
2021-11-03 12:56:07 -0700

    update to move old code

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+
+
+import pandas
+
+# using valuecounts method
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray = data["Primary Fur Color"].value_counts()
+new_file = pandas.DataFrame(gray)
+new_file.to_csv("squirrel_count.csv")
+
+#using class solution
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_count, red_count, black_count]
+}
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count2.csv")
+
+# original project code without list comprehension
+
 # # with open("weather_data.csv") as data_file:
 # #     data = data_file.readlines()
 # #     print(data)
@@ -37,29 +62,6 @@
 # print(monday_temp)
 #
 # print(monday_temp)
-
-import pandas
-
-# using valuecounts method
-data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-gray = data["Primary Fur Color"].value_counts()
-new_file = pandas.DataFrame(gray)
-new_file.to_csv("squirrel_count.csv")
-
-#using class solution
-data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-gray_count = len(data[data["Primary Fur Color"] == "Gray"])
-red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_count = len(data[data["Primary Fur Color"] == "Black"])
-
-data_dict = {
-    "Fur Color": ["Gray", "Cinnamon", "Black"],
-    "Count": [gray_count, red_count, black_count]
-}
-df = pandas.DataFrame(data_dict)
-df.to_csv("squirrel_count2.csv")
-
-
 
 
 
